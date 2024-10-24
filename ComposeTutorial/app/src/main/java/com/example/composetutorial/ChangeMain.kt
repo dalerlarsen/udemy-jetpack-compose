ae7e6300c01de7fb90ea7f491835c29a163fc3d3
Dale Larsen <dale.larsen@pinnsg.com>
2024-10-24 10:03:25 -0400

    Coding Challenge - Giving Change

---
+++ ComposeTutorial/app/src/main/java/com/example/composetutorial/ChangeMain.kt
@@ -0,0 +1,46 @@
+package com.example.composetutorial
+
+
+fun main() {
+    calculateChange("14.95", "15.0")
+    calculateChange("28.32", "200.0")
+    calculateChange("51.11", "75.0")
+
+}
+
+fun calculateChange(priceStr: String, cashStr: String) {
+    val denominations = listOf(100.0, 50.0, 20.0, 10.0, 5.0, 1.0, 0.25, 0.10, 0.05, 0.01)
+    val denominationNames = listOf(
+        "HUNDRED",
+        "FIFTY",
+        "TWENTY",
+        "TEN",
+        "FIVE",
+        "ONE",
+        "QUARTER",
+        "DIME",
+        "NICKEL",
+        "PENNY",
+    )
+
+    println("---------------------------------")
+    println("Calculate change: price=$priceStr cash=$cashStr")
+    val price = priceStr.toDouble()
+    val cash = cashStr.toDouble()
+
+    var changeAmount = cash - price
+    println(changeAmount)
+
+    val change = mutableMapOf<String, Int>()
+
+    denominations.forEachIndexed { index, denomination ->
+        val count = (changeAmount / denomination).toInt()
+        if (count > 0) {
+            change[denominationNames[index]] = count
+            changeAmount -= count * denomination
+        }
+    }
+
+    println(change)
+    println()
+}
